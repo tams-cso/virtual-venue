15d4c30096de3f495f64d9e04e3b8390b1e2504e
Michael Zhao <michaelzhao314@gmail.com>
2020-09-18 21:04:28 -0500

    Keyboard movements!

--- src/app.js
+++ src/app.js
@@ -11,6 +11,49 @@ app.use('/client', express.static(__dirname + '/client'));
 
 server.listen(8080, () => console.log("Listening on port 8080!"));
 
+var socketList = {};
+var keys = {};
+var players = {};
+var count = 0;
+const SPEED = 15;
+
 io.sockets.on('connection', function(socket) {
-    console.log('socket connected');
+    console.log('user connected!');
+
+    socket.id = count++;
+    keys[socket.id] = {};
+    players[socket.id] = { x: 500, y: 500, color: randInt(0, 16777215).toString(16) };
+    console.log(players[socket.id].color);
+    socketList[socket.id] = socket;
+    
+    socket.on('keydown', (key) => {
+        keys[socket.id][key.toLowerCase()] = true;
+    });
+
+    socket.on('keyup', (key) => {
+        keys[socket.id][key.toLowerCase()] = false;
+    });
+
+    socket.on('disconnect', () => {
+        delete socketList[socket.id];
+        delete keys[socket.id];
+        delete players[socket.id];
+        console.log('user disconnected!');
+    })
 })
+
+setInterval(function() {
+    for (var i in socketList) {
+        var socket = socketList[i];
+        var keyDown = keys[socket.id];
+        if (keyDown['w'] || keyDown['arrowup']) players[socket.id].y -= SPEED;
+        if (keyDown['s'] || keyDown['arrowdown']) players[socket.id].y += SPEED;
+        if (keyDown['a'] || keyDown['arrowleft']) players[socket.id].x -= SPEED;
+        if (keyDown['d'] || keyDown['arrowright']) players[socket.id].x += SPEED;
+    }
+    io.emit('update', players);
+}, 1000/20);
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
--- src/client/js/Game.js
+++ src/client/js/Game.js
@@ -1,27 +1,72 @@
 var socket = io();
 
-var center = {};
+var center = { x: 0, y: 0 };
+var players = [];
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-ctx.font = '100px Arial';
 
+function setup() {
+    resize();
+    window.addEventListener('resize', resize);
+    console.log('game setup!');
+
+    window.addEventListener('keydown', (event) => {
+        var key = event.key.toLowerCase();
+        if (
+            key == 'w' ||
+            key == 's' ||
+            key == 'a' ||
+            key == 'd' ||
+            key == 'arrowup' ||
+            key == 'arrowdown' ||
+            key == 'arrowleft' ||
+            key == 'arrowright'
+        )
+            socket.emit('keydown', key);
+    });
+
+    window.addEventListener('keyup', (event) => {
+        var key = event.key.toLowerCase();
+        if (
+            key == 'w' ||
+            key == 's' ||
+            key == 'a' ||
+            key == 'd' ||
+            key == 'arrowup' ||
+            key == 'arrowdown' ||
+            key == 'arrowleft' ||
+            key == 'arrowright'
+        )
+            socket.emit('keyup', key);
+    });
+
+    socket.on('update', (players) => {
+        draw(players);
+    });
+}
+
+// Function to keep the canvas centered and fullscreened
+// and is called whenever the viewport size changes
 function resize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     center = {
         x: canvas.width / 2,
-        y: canvas.height / 2
-    }
+        y: canvas.height / 2,
+    };
     draw();
 }
 
-function draw() {
-    ctx.fillStyle = "#ffd700";
-    ctx.fillRect(center.x - 100, center.y - 100, 200, 200);
-    ctx.font = "30px Comic Sans MS";
-    ctx.fillStyle = "#000000";
-    ctx.fillText("Hello World!", center.x - 80, center.y);
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
-resize();
-window.addEventListener('resize', resize);
+function draw(players) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (var i in players) {
+        var p = players[i];
+
+        ctx.fillStyle = '#' + p.color.padStart(6, '0');
+        ctx.fillRect(p.x, p.y, 30, 30);
+    }
+}

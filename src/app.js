67e099d48f5d302e41418cdf4fece7b3beffe181
Michael Zhao <michaelzhao314@gmail.com>
2020-09-19 21:55:58 -0500

    Moved mvt calc to client side

--- src/app.js
+++ src/app.js
@@ -13,47 +13,29 @@ app.use('/client', express.static(__dirname + '/client'));
 server.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
 
 var socketList = {};
-var keys = {};
 var players = {};
 var count = 0;
-const SPEED = 15;
 
 io.sockets.on('connection', function(socket) {
     console.log('user connected!');
 
     socket.id = count++;
-    keys[socket.id] = {};
     players[socket.id] = { x: 500, y: 500, color: randInt(0, 16777215).toString(16).padStart(6, '0') };
-    console.log(players[socket.id].color);
     socketList[socket.id] = socket;
+    socket.emit("start", {socketId: socket.id, players});
     
-    socket.on('keydown', (key) => {
-        keys[socket.id][key.toLowerCase()] = true;
-    });
-
-    socket.on('keyup', (key) => {
-        keys[socket.id][key.toLowerCase()] = false;
+    socket.on('move', (movedPlayer) => {
+        players[socket.id] = movedPlayer;
+        io.emit('update', players);
     });
 
     socket.on('disconnect', () => {
         delete socketList[socket.id];
-        delete keys[socket.id];
         delete players[socket.id];
+        io.emit('update', players);
         console.log('user disconnected!');
     })
 })
-
-setInterval(function() {
-    for (var i in socketList) {
-        var socket = socketList[i];
-        var keyDown = keys[socket.id];
-        if (keyDown['w'] || keyDown['arrowup']) players[socket.id].y -= SPEED;
-        if (keyDown['s'] || keyDown['arrowdown']) players[socket.id].y += SPEED;
-        if (keyDown['a'] || keyDown['arrowleft']) players[socket.id].x -= SPEED;
-        if (keyDown['d'] || keyDown['arrowright']) players[socket.id].x += SPEED;
-    }
-    io.emit('update', players);
-}, 1000/20);
 
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
--- src/client/js/Game.js
+++ src/client/js/Game.js
@@ -1,9 +1,13 @@
 var socket = io();
+const FPS = 20;
+const SPEED = 15;
 
 var center = { x: 0, y: 0 };
-var players = [];
+var keyList = {};
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+var currPlayer;
+var socketId;
 
 function setup() {
     resize();
@@ -11,37 +15,46 @@ function setup() {
     console.log('game setup!');
 
     window.addEventListener('keydown', (event) => {
-        var key = event.key.toLowerCase();
-        if (
-            key == 'w' ||
-            key == 's' ||
-            key == 'a' ||
-            key == 'd' ||
-            key == 'arrowup' ||
-            key == 'arrowdown' ||
-            key == 'arrowleft' ||
-            key == 'arrowright'
-        )
-            socket.emit('keydown', key);
+        keyList[event.key.toLowerCase()] = true;
     });
 
     window.addEventListener('keyup', (event) => {
-        var key = event.key.toLowerCase();
-        if (
-            key == 'w' ||
-            key == 's' ||
-            key == 'a' ||
-            key == 'd' ||
-            key == 'arrowup' ||
-            key == 'arrowdown' ||
-            key == 'arrowleft' ||
-            key == 'arrowright'
-        )
-            socket.emit('keyup', key);
+        keyList[event.key.toLowerCase()] = false;
     });
 
     socket.on('update', (players) => {
+        currPlayer = players[socketId];
         draw(players);
+    });
+
+    socket.on('start', (data) => {
+        socketId = data.socketId;
+        currPlayer = data.players[data.socketId];
+        draw(data.players);
+
+        setInterval(() => {
+            var change = false;
+            if (keyList['w'] || keyList['arrowup']) {
+                currPlayer.y -= SPEED;
+                change = true;
+            }
+            if (keyList['s'] || keyList['arrowdown']) {
+                currPlayer.y += SPEED;
+                change = true;
+            }
+            if (keyList['a'] || keyList['arrowleft']) {
+                currPlayer.x -= SPEED;
+                change = true;
+            }
+            if (keyList['d'] || keyList['arrowright']) {
+                currPlayer.x += SPEED;
+                change = true;
+            }
+
+            if (change) {
+                socket.emit('move', currPlayer);
+            }
+        }, 1000 / FPS);
     });
 }
 
